b3d9f670026c89983b02a96f0e91eb43caf9bd09
dominopiece <dominopiece@gmail.com>
2023-02-20 00:05:33 +0900

    setup router #1

---
+++ movie-app/src/routes/Home.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from "react";
+import Movie from "./components/Movie";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  // useEffect(() => {
+  //   fetch( //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+  //   )
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // async await
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
+      )
+    ).json();
+    // const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      <h1>Movie Rating: Top {movies.length}</h1>
+      {/* <li>
+        {movies.map((item) => (
+          <ol key={item.id}>{item.url}</ol>
+        ))}
+      </li> */}
+      {loading ? (
+        <h1>Loading..</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              rating={movie.rating}
+              year={movie.year}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
